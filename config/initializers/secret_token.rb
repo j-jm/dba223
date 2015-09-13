7d2e210aaea2af9051d56cc696befcc6c02ff7fa
Jean-Jacques Maury <jean0jacques@yahoo.com>
2015-09-13 09:55:29 -0400

    Initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ch3CodeEx::Application.config.secret_token = '6cf92bbf55797598bd6dfabab132973de050f499ec350dd9a4863818fe77ecd360bcbe37ca8b079f392a6c900097b3940b1995acd383f5db0d2acd162dd59006'
